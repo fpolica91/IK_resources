5ad2a70a0bb0bab5169585ea60e26d7119797fdc
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-12-31 22:52:05 -0500

    i think i got it

--- educative/js/find_word_concatenation.js
+++ educative/js/find_word_concatenation.js
@@ -18,6 +18,7 @@ function find_word_concatenation(str, arr) {
     const wordsSeen = {};
     for (let j = 0; j < wordscount; j++) {
       wordIndex = i + j * eachWordLength;
+
       word = str.substring(wordIndex, wordIndex + eachWordLength);
       if (!(word in wordFrequency)) {
         break;
@@ -29,6 +30,7 @@ function find_word_concatenation(str, arr) {
       wordsSeen[word]++;
 
       if (wordsSeen[word] > wordFrequency[word] || 0) {
+        console.log(j, "not here");
         break;
       }
 
@@ -40,36 +42,6 @@ function find_word_concatenation(str, arr) {
   return results;
 }
 
-function solve(str, list) {
-  const charmap = {};
-  let res = "";
-  let start = 0;
-  let results = [];
-  let key_1 = [...list].reverse().join("");
-  let key_2 = list.join("");
-  if (!(key_1 in charmap)) {
-    charmap[key_1] = 0;
-  }
-  if (!(key_2 in charmap)) {
-    charmap[key_2] = 0;
-  }
-  charmap[key_1]++;
-  charmap[key_2]++;
-  for (let end = 0; end < str.length; end++) {
-    const rightchar = str[end];
-    res += rightchar;
-    while (res.length >= key_1.length) {
-      if (res in charmap) {
-        results.push(start);
-      }
-      start++;
-      res = str.substring(start, start + key_1.length);
-    }
-  }
-  return results;
-}
-// console.log(find_word_concatenation("catfoxcat", ["cat", "fox"]));
-console.log(solve("catfoxcat", ["cat", "fox"]));
-console.log(solve("catcatfoxfox", ["cat", "fox"]));
-console.log(solve("cmapcapmap", ["cap", "map"]));
+console.log(find_word_concatenation("catfoxcat", ["cat", "fox"]));
 // console.log(find_word_concatenation("catcatfoxfox", ["cat", "fox"]));
+// console.log(find_word_concatenation("cmapcapmap", ["cap", "map"]));
---
+++ educative/js/find_word_concatenation2.js
@@ -0,0 +1,32 @@
+function solve(str, list) {
+  const charmap = {};
+  let res = "";
+  let start = 0;
+  let results = [];
+  let key_1 = [...list].reverse().join("");
+  let key_2 = list.join("");
+  if (!(key_1 in charmap)) {
+    charmap[key_1] = 0;
+  }
+  if (!(key_2 in charmap)) {
+    charmap[key_2] = 0;
+  }
+  charmap[key_1]++;
+  charmap[key_2]++;
+  for (let end = 0; end < str.length; end++) {
+    const rightchar = str[end];
+    res += rightchar;
+    while (res.length >= key_1.length) {
+      if (res in charmap) {
+        results.push(start);
+      }
+      start++;
+      res = str.substring(start, start + key_1.length);
+    }
+  }
+  return results;
+}
+
+console.log(solve("catfoxcat", ["cat", "fox"]));
+console.log(solve("catcatfoxfox", ["cat", "fox"]));
+console.log(solve("cmapcapmap", ["cap", "map"]));
